e89f6e86535aeb55343582b9eaf737b737ba9578
sakuramilk <c.sakuramilk@gmail.com>
2015-06-05 23:56:55 +0900

    change remote url

--- gsync.sh
+++ gsync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -f .gclient ]; then
-  gclient config https://github.com/kbc-developers/mozc.git --name=. --deps-file=src/DEPS
+  gclient config https://github.com/kbc-developers/android_packages_inputmethods_Mozc.git --name=. --deps-file=src/DEPS
 fi
 
 gclient sync
