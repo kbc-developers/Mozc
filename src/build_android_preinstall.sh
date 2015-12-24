2cd9c86cc835025cc93b53ca668ef740a5835cc1
ma34s <ma34s.sss@gmail.com>
2015-12-25 02:15:41 +0900

    signed key changed to platform

--- src/build_android_preinstall.sh
+++ src/build_android_preinstall.sh
@@ -5,12 +5,16 @@ input_apk=android/bin/MozcForAndroid-release-unsigned.apk
 signed_apk=android/bin/MozcForAndroid-release-signed.apk
 release_apk=android/bin/MozcForAndroid.apk
 
-if [ -f $input_apk ]; then
-  rm $input_apk
+if [ -d out_android ]; then
+  rm -R out_android
 fi
 if [ -f $signed_apk ]; then
   rm $signed_apk
 fi
+if [ -f $release_apk ]; then
+  rm $release_apk
+fi
+
 if [ -f $release_apk ]; then
   rm $release_apk
 fi
@@ -21,6 +25,6 @@ python build_mozc.py build -c Release android/android.gyp:apk
 if [ ! -f $input_apk ]; then
 	exit 1
 fi
-java -jar $release_tools/signapk.jar $release_tools/testkey.x509.pem $release_tools/testkey.pk8 $input_apk $signed_apk
+java -jar $release_tools/signapk.jar $release_tools/platform.x509.pem $release_tools/platform.pk8 $input_apk $signed_apk
 echo zipalign_process
 $zipalign -v 4 $signed_apk $release_apk
