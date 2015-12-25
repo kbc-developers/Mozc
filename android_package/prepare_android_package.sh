3eeb0bf114a0a64b1fb089c50b3a99184c356d97
ma34s <ma34s.sss@gmail.com>
2015-12-25 12:33:25 +0900

    update build script

--- android_package/prepare_android_package.sh
+++ android_package/prepare_android_package.sh
@@ -3,7 +3,21 @@
 curr=`pwd`
 
 cd ../src
-./build_android_preinstall.sh
+input_apk=android/bin/MozcForAndroid-release-unsigned.apk
+
+if [ -d out_android ]; then
+  rm -R out_android
+fi
+
+#if use preinstall apk, it does not change android_application_id right now.
+#python build_mozc.py gyp --target_platform=Android --android_application_id=org.kbc_brick.mozc --android_hide_icon=1
+python build_mozc.py gyp --target_platform=Android --android_hide_icon=1
+python build_mozc.py build -c Release android/android.gyp:apk
+
+if [ ! -f $input_apk ]; then
+	exit 1
+fi
+
 
 cd $curr
 ./copy_preinstall.sh
